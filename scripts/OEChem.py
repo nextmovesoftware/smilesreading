3d4c0ae0247d66392e6543c133dd332f33860edb
Noel O'Boyle <baoilleach@gmail.com>
2017-09-12 16:03:08 +0100

    Add OEChem

---
+++ scripts/OEChem.py
@@ -0,0 +1,27 @@
+import sys
+from openeye import oechem as oe
+
+import common
+
+class MyAromaticSmilesWriter(common.AromaticSmilesWriter):
+    def getoutput(self, smi):
+        mol = oe.OEGraphMol()
+        ok = oe.OEParseSmiles(mol, smi)
+        assert ok
+        oe.OEAssignAromaticFlags(mol)
+        return oe.OECreateSmiString(mol, 0)
+
+class MyHydrogenCounter(common.HydrogenCounter):
+    def getoutput(self, smi):
+        mol = oe.OEGraphMol()
+        ok = oe.OEParseSmiles(mol, smi)
+        if not ok:
+            return None, "Kekulization_failure"
+
+        return [atom.GetImplicitHCount() for atom in mol.GetAtoms()]
+
+if __name__ == "__main__":
+    myname = "oechem_June2017"
+    MyAromaticSmilesWriter(myname).main()
+    # MyHydrogenCounter(myname).main()
+
