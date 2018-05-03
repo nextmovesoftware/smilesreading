c6596b07402eb3fe035f4fd5718e1430b7237965
Noel O'Boyle <baoilleach@gmail.com>
2018-05-03 14:13:59 +0100

    Start updating OpenChemLib for 2018.5.0. Now can create aromatic SMILES.

--- scripts/OpenChemLib.py
+++ scripts/OpenChemLib.py
@@ -4,12 +4,21 @@ import com.actelion.research.chem as chem
 
 import common
 
+class MyAromaticSmilesWriter(common.AromaticSmilesWriter):
+    def getoutput(self, smi):
+        parser = chem.SmilesParser()
+        mol = chem.StereoMolecule()
+        parser.parse(mol, smi, java.lang.Boolean(False), java.lang.Boolean(False))
+
+        isc = chem.IsomericSmilesCreator(mol)
+        return isc.getSmiles()
+
 class MyHydrogenCounter(common.HydrogenCounter):
     def getoutput(self, smi):
         parser = chem.SmilesParser()
         mol = chem.StereoMolecule()
         try:
-            parser.parse(mol, smi, False, False)
+            parser.parse(mol, smi, java.lang.Boolean(False), java.lang.Boolean(False))
         except java.lang.Exception, e:
             if "Assignment of aromatic double bonds" in e.message:
                 return None, "Kekulization_failure"
@@ -20,6 +29,7 @@ class MyHydrogenCounter(common.HydrogenCounter):
         return hcounts, None
 
 if __name__ == "__main__":
-    myname = "openchemlib_2017.9.0"
+    myname = "openchemlib_2018.5.0"
+    # MyAromaticSmilesWriter(myname).main()
     MyHydrogenCounter(myname).main()
 
